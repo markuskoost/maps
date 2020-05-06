e1bdda38094c38cef814e830e5a523cec9f8fc9e
markus.koost <markuskoost@gmail.com>
2020-05-06 12:16:22 +0300

    misc

--- connection.php
+++ connection.php
@@ -1,23 +1,5 @@
 <?php
-//$servername = "d82729.mysql.zonevs.eu";
-//$username = "d82729sa337621";
-//$password = "mapsmaps123";
-//$dbname = "d82729_maps";
-//
-//try {
-//    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-//    // set the PDO error mode to exception
-//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//
-////    $referer = $_SERVER['HTTP_REFERER'];
-////    header("Location: $referer");
-//
-//    echo "Connected successfully";
-//}
-//catch(PDOException $e)
-//{
-//    echo "Connection failed: " . $e->getMessage();
-//}
+
 $mysqli = new mysqli(
     "x",
     "x",
@@ -30,7 +12,3 @@ if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-
-//$referer = $_SERVER['HTTP_REFERER'];
-//header("Location: $referer");
-//header("Content-type: text/xml");
